84277ec0c59532695eef44333a50ca56cd443ef7
reed <reed.tseng@outlook.com>
2022-04-28 16:46:28 +0800

    init

---
+++ all.js
@@ -0,0 +1,17 @@
+const products = [
+    {
+        name: '鍋燒意麵',
+        price: 100,
+    },
+    {
+        name: '韓式奶油鍋鍋燒意麵',
+        price: 120,
+    },
+    {
+        name: '泡菜鍋燒意麵',
+        price: 115,
+    }
+];
+
+let shoppingCart = [];
+let total = 0;
